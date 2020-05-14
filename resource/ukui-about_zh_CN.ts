74720a9b2b5b5e1d9f63dda2857b6b18e08490ba
hepuyao <2410474020@qq.com>
2020-05-14 09:30:39 +0800

    add ts qm

---
+++ resource/ukui-about_zh_CN.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="../widget.ui" line="14"/>
+        <source>Widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../widget.ui" line="26"/>
+        <location filename="../widget.ui" line="39"/>
+        <location filename="../widget.ui" line="52"/>
+        <location filename="../widget.ui" line="65"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="316"/>
+        <location filename="../widget.cpp" line="322"/>
+        <source>All rights reserved by 2009-2020 KylinOS. all rights reserved.
+ Kylin %1 and its user interface is protected by intellectual property laws trademark law in China and other countries and other regions to be enacted or enacted.</source>
+        <translation type="unfinished">版权所有©2009-2020 KylinOS.保留所有权利。
+Kylin %1版本及其用户界面受中国及其它国家地区的商标法和其它待颁布或已颁布的知识产权法保护。</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="323"/>
+        <source>版权所有©2009-2020 KylinOS.保留所有权利。
+ Kylin %1版本及其用户界面受中国及其它国家地区的商标法和其它待颁布或已颁布的知识产权法保护</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="329"/>
+        <source>All rights reserved by 2009-2020 YHKylinOS. all rights reserved. 
+ YHKylin version 4 and its user interface is protected by intellectual property laws trademark law in China and other countries and other regions to be enacted or enacted.</source>
+        <translation>版权所有©2009-2020 YHKylinOS.保留所有权利。
+ YHKylin V4版及其用户界面受中国及其它国家地区的商标法和其它待颁布或已颁布的知识产权法保护。</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="335"/>
+        <source>All rights reserved by 2009-2020 NeoKylinOS. all rights reserved.
+ NeoKylin version 10 and its user interface is protected by intellectual property laws trademark law in China and other countries and other regions to be enacted or enacted.</source>
+        <translation>版权所有©2009-2020 NeoKylinOS.保留所有权利。
+NeoKylin V10版及其用户界面受中国及其它国家地区的商标法和其它待颁布或已颁布的知识产权法保护。</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="367"/>
+        <source>about Kylin</source>
+        <translation>关于银河麒麟</translation>
+    </message>
+</context>
+</TS>
